c25d6ed65c4722c521f9858541a456877c00c5ce
Alejandro Pérez <jandropehe4@gmail.com>
2024-11-28 10:31:04 +0100

    added forgot password

--- app/src/main/java/com/crraul/nicestart/Login.java
+++ app/src/main/java/com/crraul/nicestart/Login.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -16,6 +19,8 @@ import androidx.core.view.WindowInsetsCompat;
 // lo del glide
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.snackbar.Snackbar;
 
 public class Login extends AppCompatActivity {
     @Override
@@ -30,6 +35,40 @@ public class Login extends AppCompatActivity {
             return insets;
         });
 
+    }
+
+    public void showAlertDialogButtonClicked(Login mainActivity) {
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(this);
+
+        builder.setTitle("Forgot Password?");
+        builder.setIcon(R.drawable.key);
+        builder.setCancelable(true);
+
+        // sends to profile activity
+        builder.setPositiveButton("Reset Password", (dialogInterface, i) -> {
+            final ConstraintLayout layout = findViewById(R.id.main);
+
+            Snackbar snackbar = Snackbar.make(layout, "We've sent a password reset link to your email. " +
+                                                            "Please check your inbox and follow the instructions.",
+                                                            Snackbar.LENGTH_SHORT);
+//                    .setAction("UNDO", view -> {
+//                        Snackbar snackbar1 = Snackbar.make(layout, "Action is restored", Snackbar.LENGTH_SHORT);
+//                        snackbar1.show();
+//                    });
+            snackbar.show();
+
+            dialogInterface.dismiss();
+        });
+
+        // does nothing
+        builder.setNegativeButton("Return Login", (dialogInterface, i) -> dialogInterface.dismiss());
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    public void forgotPassword(View v) {
+        showAlertDialogButtonClicked(this);
     }
 
     public void openSignup(View v) {
